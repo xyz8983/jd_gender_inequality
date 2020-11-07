684e28dd7736edd98d9d0e06f79b71296067b392
Yuhang Zheng <yzheng83@massmutual.com>
2020-11-07 10:42:29 -0500

    update item

--- jd_spider/jd_spider/items.py
+++ jd_spider/jd_spider/items.py
@@ -9,9 +9,9 @@ from scrapy.item import Field
 class JDItem(scrapy.Item):
     url = Field() # str
     company_name = Field() # str
-    company_type = Field() # str # nullable
-    company_size = Field() # str # nullable, detailed address
-    company_industry = Field() # str # nullable
+    company_type = Field() # str # nullable for example: 民营公司
+    company_size = Field() # str # nullable, detailed address 
+    company_industry = Field() # str # nullable example: 互联网/电子商务 批发/零售
     company_address = Field() # str
     city = Field() # str
     district = Field() # str # nullable
@@ -22,7 +22,7 @@ class JDItem(scrapy.Item):
     descrimination_content = Field() # str
     # TODO: first 2-3 keywords are enough
     keywords = Field() #[str]  # nullable, for example: 职位标签, 关键字 
-    salary_range = Field() # str # TODO: havent decided what it should look like
+    salary_range = Field() # str #nullable TODO: havent decided what it should look like
     gender_preference = Field() # str, an aggregation field
     age_requirement = Field() # str # nullable # TODO: needs extra cleanning
     jd_date = Field() # str, when the jd is published
