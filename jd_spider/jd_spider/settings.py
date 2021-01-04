8f005a2e916084e840d11a989c38c3e811133f69
Yuhang Zheng <yzheng83@massmutual.com>
2021-01-04 12:12:36 -0500

    update setting

--- jd_spider/jd_spider/settings.py
+++ jd_spider/jd_spider/settings.py
@@ -17,16 +17,16 @@ NEWSPIDER_MODULE = 'jd_spider.spiders'
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
-# ROBOTSTXT_OBEY = False
+# ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 4
+CONCURRENT_REQUESTS = 2
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 6
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
