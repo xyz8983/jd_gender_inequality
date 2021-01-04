d064cdc2bdcc3b4d63cb2a9f685f913ec639d373
Yuhang Zheng <yzheng83@massmutual.com>
2021-01-04 12:13:06 -0500

    add test case

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,12 @@
 import pytest
-from ..src.analysis_utils import find_age, find_outlooking
+from jd_gender_inequality.src.analysis_utils import (  
+    find_age, 
+    find_outlooking,
+    company_size_clean,
+    zhuobo_salary_clean,
+    job_title_cleans
+)
+import numpy as np
 
 @pytest.mark.parametrize(
     ["text","ex_result"],
@@ -16,6 +23,28 @@ def test_find_age(text, ex_result):
     result = find_age(text)
     assert result == ex_result, f"return {result} but expecte {ex_result}"
     
+
+@pytest.mark.parametrize(
+    ["s", "exp_res"],
+    [
+        ('少于50人', (0, 50)),
+        ('10000人以上', (10000, 0)),
+        (None, (0, 0) ),
+        ('50-150人', (50, 150))
+    ]
+)
+def test_wuyi_company_size_clean(s, exp_res):
+    processed_s = company_size_clean(s)
+    assert processed_s==exp_res, f'expcted result is {exp_res}, but get {processed_s}'
     
-def test_find_outlooking():
-    pass
+    
+@pytest.mark.parametrize(
+    ["s", "exp_res"],
+    [
+        ('¥4.5-6K', (4500, 6000)),
+        ('', (np.nan, np.nan))
+    ]
+)    
+def test_zhuobo_salary_clean(s, exp_res):
+    processed_s = zhuobo_salary_clean(s)
+    assert processed_s == exp_res, f'expcted result is {exp_res}, but get {processed_s}'
